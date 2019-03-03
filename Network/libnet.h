e5ab95650737c7e19e04bb07448cbed0dc0746ad
Hind Malti <hmalti@polytech-lille.net>
2019-03-03 16:59:08 +0100

    Ajout signature boucleServeur à libnet.h

--- Network/libnet.h
+++ Network/libnet.h
@@ -2,5 +2,6 @@
 #define LIBNET_H
 int sendUDPBroadcast(char *message, int port);
 int sendUDPUnicast(char *address, char *message, int port);
+int boucleServeur(int, void (*)(int));
 #define MAX_TCP_CONNEXION 20
 #endif
